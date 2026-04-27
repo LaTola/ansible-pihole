48725ba7e792cbf68b87791da763f784730fcbf9
Wilbert Iglesias <wilbert.iglesias@angi.com>
2026-04-27 09:15:55 -0300

    Remove unbound service control from optimize_FTL.sh and clean up comments

--- roles/configure_cron/files/optimize_FTL.sh
+++ roles/configure_cron/files/optimize_FTL.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 cd /etc/pihole
-systemctl stop unbound
 systemctl stop pihole-FTL
 rm gravity_old.db
 sync
 for db in $(ls *.db)
 do
-  # Enable WAL and full optimization
+  # Full optimization
   sqlite3 $db "PRAGMA integrity_check; VACUUM; REINDEX; ANALYZE;"
 done
 sync
-systemctl start unbound
 systemctl start pihole-FTL
